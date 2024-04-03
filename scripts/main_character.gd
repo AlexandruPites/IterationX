03f873b3a5f084490a6d967a92c9841c59ca6276
AlexandruPites <pitesalexandru@gmail.com>
2024-04-03 09:56:46 +0300

    defined custom input events, movement logic is now in _process()

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,32 +1,27 @@
 extends CharacterBody2D
 
 #constants
-const SPEED = 300.0
+const SPEED : float = 300.0
 
 #player attributes
-var health = 100
-var is_left = false
+var health : float = 100.0
 @onready var sprite_2d = $Sprite2D
 
 
-func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
-	var vertical_direction = Input.get_axis("ui_up", "ui_down")
+func _process(delta):
+	
+	var horizontal_direction = Input.get_axis("left", "right")
+	var vertical_direction = Input.get_axis("up", "down")
 	
 	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
-		is_left = velocity.x < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	sprite_2d.flip_h = is_left
-		
+		position.x += horizontal_direction * SPEED * delta
+	
+	if horizontal_direction < 0:
+		sprite_2d.flip_h = true
+	elif horizontal_direction > 0:
+		sprite_2d.flip_h = false
+	
 	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+		position.y += vertical_direction * SPEED * delta
 	
 	move_and_slide()
