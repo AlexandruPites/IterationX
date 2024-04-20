78b5fb81f5f0cf77a2b48dd67ea7b97fd2349ff0
AlexandruPites <pitesalexandru@gmail.com>
2024-04-20 21:15:37 +0300

    added penetration

--- scripts/4-way.gd
+++ scripts/4-way.gd
@@ -3,6 +3,7 @@ extends Projectile
 var projectile_speed : float = 1000
 var velocity : Vector2 = Vector2(projectile_speed, projectile_speed)
 var damage : float = 10.0
+var hit_counter : int = 5
 
 func _process(delta: float) -> void:
 	position += velocity * delta
@@ -11,4 +12,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("damageable"):
 		var direction : Vector2 = global_position.direction_to(body.global_position).normalized()
 		body.take_damage(damage, direction)
-		queue_free()
+		hit_counter -= 1
+		if hit_counter <= 0:
+			queue_free()
