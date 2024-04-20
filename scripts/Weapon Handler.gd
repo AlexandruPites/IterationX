e79dbdcaae3ac18f6222dea2c08a1da8d1f1dfab
AlexandruPites <pitesalexandru@gmail.com>
2024-04-20 17:28:08 +0300

    Player can shoot

---
+++ scripts/Weapon Handler.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name WeaponHandler
+
+
+const PROJECTILE = preload("res://scenes/projectile.tscn")
+var inventory : Array[Resource] = [PROJECTILE]
+@onready var player: Player = $".."
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	print(player)
+	if Input.is_action_just_pressed("accept"):
+		print("shoot")
+		shoot()
+	
+func shoot() -> void:
+	for weapon in inventory:
+		var instance : Projectile = PROJECTILE.instantiate()
+		instance.position = get_parent().position
+		if player.is_left():
+			instance.velocity = -instance.velocity
+		get_node("/root/Game").add_child(instance)
