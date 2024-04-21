1c14b1f68e361f91c1ba45f53095055142d0b3a3
Padurariu <theophille.developing@gmail.com>
2024-04-21 13:41:54 +0300

    XP Capsule Added

--- scripts/4-way.gd
+++ scripts/4-way.gd
@@ -2,7 +2,7 @@ extends Projectile
 
 var projectile_speed : float = 1000
 var velocity : Vector2 = Vector2(projectile_speed, projectile_speed)
-var damage : float = 10.0
+var damage : float = 50.0
 var hit_counter : int = 5
 
 func _process(delta: float) -> void:
