3937819f67daba02a49209cbfd536594a181dbc2
AlexandruPites <pitesalexandru@gmail.com>
2024-05-11 18:20:21 +0300

    lightning field

---
+++ scripts/lightning_fieldHandler.gd
@@ -0,0 +1,49 @@
+extends Node
+
+var weapon_name : String = "lightning-field"
+var format : String = "res://scenes/weapons/%s/%s.tscn"
+var resource : Resource
+
+const FIXED_SCALE : float = 2
+
+var level : int = 1
+var base_damage : float = 10
+var base_area : float = 2
+
+var damage : float
+var swing_speed : float
+var player: Player
+var field : Projectile
+var area : float
+@onready var timer: Timer = $Timer
+
+var modifiers : StatIncrease = StatIncrease.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	resource = load(format % [weapon_name, weapon_name])
+	calc_stats()
+	
+	field = resource.instantiate()
+	field.position = player.position
+	set_stats()
+	add_child(field)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	field.position = player.position
+
+func set_stats() -> void:
+	field.level = level
+	field.damage = damage
+	field.area = area
+	field.scale = Vector2(area, area)
+
+func calc_stats() -> void:
+	damage = base_damage * level * (1 + modifiers.damage_increase)
+	area = FIXED_SCALE * base_area * (1 + float(level) / 9) * (1 + modifiers.area_increase)
+	
+func update_stats(new_level : int) -> void:
+	level = new_level
+	calc_stats()
+	set_stats()
