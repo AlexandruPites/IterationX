8aa84630e891e8f553e423b4fd635dcdb7db49bd
luca.pana <luca.pana@stud.acs.upb.ro>
2024-05-11 17:42:25 +0300

    injector menu sketch

--- scripts/CanvasLayer.gd
+++ scripts/CanvasLayer.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 @onready var settings: Control = $Settings
 @onready var main_menu: Control = $Main
+@onready var injector: Control = $Injector
 
 @onready var button_back: Button = $Settings/CenterContainer/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/ButtonBack
 @onready var button_play: Button = $Main/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ButtonPlay
+@onready var button_injector: Button = $Main/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ButtonInjector
+@onready var back_from_injector_to_main: Button = $Injector/BackFromInjectorToMain
 
 
 func _on_button_play_pressed() -> void:
@@ -25,3 +28,15 @@ func _on_button_back_pressed() -> void:
 	main_menu.visible = true
 	settings.visible = false
 	button_play.grab_focus.call_deferred()
+
+
+func _on_button_injector_pressed() -> void:
+	main_menu.visible = false
+	injector.visible = true
+
+
+func _on_back_from_injector_to_main_pressed() -> void:
+	main_menu.visible = true
+	injector.visible = false
+	button_play.grab_focus.call_deferred()
+
