d2eb84769b8ad5e3e5c9eda8d5ac6ead0cfcda74
AndreiCalcan <calcan.ac55@gmail.com>
2024-05-13 22:29:23 +0300

    Added ranged enemy spawn logic

--- scripts/EnemySpawner.gd
+++ scripts/EnemySpawner.gd
@@ -4,7 +4,11 @@ class_name EnemySpawner
 var enemy_array : Array[Enemy] = []
 var enemy_scenes : Array[PackedScene]
 var xp_scene : Resource = preload("res://scenes/xp.tscn")
-const max_enemies : int = 5
+var ranged_enemy : PackedScene
+const max_melee: int = 5
+const max_ranged: int = 3
+var melee_count : int = 0
+var ranged_count : int = 0
 const spawn_distance : float = 900
 @onready var player : CharacterBody2D = $"../Player"
 @onready var timer : Timer = $Timer
@@ -13,13 +17,12 @@ var enemy_index : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	enemy_scenes.append(preload("res://scenes/enemies/white_robot.tscn"))
 	enemy_scenes.append(preload("res://scenes/enemies/robot.tscn"))
-	enemy_scenes.append(preload("res://scenes/enemies/zumzarel.tscn"))
-	enemy_scenes.append(preload("res://scenes/enemies/torpalod.tscn"))
+	#enemy_scenes.append(preload("res://scenes/enemies/zumzarel.tscn"))
+	#enemy_scenes.append(preload("res://scenes/enemies/torpalod.tscn"))
 	enemy_scenes.append(preload("res://scenes/enemies/green_robot.tscn"))
 	enemy_scenes.append(preload("res://scenes/enemies/don_juan.tscn"))
-	
+	ranged_enemy = preload("res://scenes/enemies/white_robot.tscn")
 	
 func _on_timer_timeout() -> void:
 	if enemy_index + 1 < enemy_scenes.size():
@@ -33,6 +36,11 @@ func _on_death(source : CharacterBody2D) -> void:
 		var xp : Area2D = xp_scene.instantiate()
 		xp.position = source.position
 		add_child.call_deferred(xp)
+	if source.is_in_group("melee") and source in enemy_array:
+		melee_count -= 1
+		print("Melee died")
+	elif source.is_in_group("ranged") and source in enemy_array:
+		ranged_count -= 1
 	enemy_array.erase(source)
 	source.alive = false
 	source.death_player.play("death")
@@ -57,11 +65,12 @@ func get_closest_enemy_to_point(source_pos : Vector2) -> Enemy:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float) -> void:
-	var new_enemy_number : int = max_enemies - enemy_array.size()
+	var new_melee : int = max_melee - melee_count
+	var new_ranged : int = max_ranged - ranged_count
 	var elapsed_time : float = timer.wait_time - timer.time_left
 	var upgraded_enemy_spawn_chance : float = elapsed_time / timer.wait_time
 	
-	for i in range(new_enemy_number):
+	for i in range(new_melee):
 		var roll : float = randf()
 		var crt_index : int = enemy_index + 1 if roll < upgraded_enemy_spawn_chance and enemy_index + 1 < enemy_scenes.size() else enemy_index
 		var new_enemy : Enemy = enemy_scenes[crt_index].instantiate()
@@ -69,3 +78,12 @@ func _process(_delta : float) -> void:
 		new_enemy.position = get_spawn_coord(player.position)
 		enemy_array.append(new_enemy)
 		add_child(new_enemy)
+		melee_count += 1
+	
+	for i in range(new_ranged):
+		var new_enemy : Enemy = ranged_enemy.instantiate()
+		new_enemy.death.connect(_on_death)
+		new_enemy.position = get_spawn_coord(player.position)
+		enemy_array.append(new_enemy)
+		add_child(new_enemy)
+		ranged_count += 1
