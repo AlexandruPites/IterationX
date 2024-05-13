b412f0364a9123d7bfea40f2fe59e71ead6678d8
AlexandruPites <pitesalexandru@gmail.com>
2024-05-13 20:05:13 +0300

    suprise

---
+++ Torpalod.gd
@@ -0,0 +1,12 @@
+extends Enemy
+class_name Torpalod
+
+var robot_speed : float = 200
+var robot_health : float = 400
+var robot_knockback_resist : float = 0.8
+
+func _ready() -> void:
+	speed = robot_speed
+	health = robot_health
+	knockback_resist = robot_knockback_resist
+	scale = Vector2(2, 2)
