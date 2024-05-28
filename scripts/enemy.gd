a14c9e66e6c682a51f35a55051cc33f326604803
AndreiCalcan <calcan.ac55@gmail.com>
2024-05-28 18:02:51 +0300

    Reworked IFrames + ak nerf

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -34,18 +34,21 @@ func _process(_delta : float) -> void:
 		velocity = diff.normalized() * speed + knockback
 		knockback = lerp(knockback, Vector2.ZERO, 0.07)
 		move_and_slide()
+		
+func proc_damage(damage : float, knockback_strength : float) -> void:
+	timer.start()
+	if not animation_player.is_playing():
+		animation_player.play("take_damage")
+	health -= damage
+	var direction : Vector2 = player.global_position.direction_to(self.global_position).normalized()
+	knockback += direction * knockback_strength * knockback_resist
+	enemy_hurt.play()
+	if health <= 0:
+		despawn()
 
 func take_damage(damage : float, knockback_strength : float) -> void:
 	if timer.is_stopped():
-		timer.start()
-		if not animation_player.is_playing():
-			animation_player.play("take_damage")
-		health -= damage
-		var direction : Vector2 = player.global_position.direction_to(self.global_position).normalized()
-		knockback += direction * knockback_strength * knockback_resist
-		enemy_hurt.play()
-		if health <= 0:
-			despawn()
+		proc_damage(damage, knockback_strength)
 
 func despawn() -> void:
 	death.emit(self)
